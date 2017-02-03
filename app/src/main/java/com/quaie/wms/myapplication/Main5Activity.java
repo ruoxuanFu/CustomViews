054f4d6a8df1d4e81054eea09d6861658b8540c6
furuoxuan <1002863705@qq.com>
2017-02-03 11:17:15 +0800

    添加一些注释

--- app/src/main/java/com/quaie/wms/myapplication/Main5Activity.java
+++ app/src/main/java/com/quaie/wms/myapplication/Main5Activity.java
@@ -3,7 +3,7 @@ package com.quaie.wms.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//对话框实例
+//对话框详解
 public class Main5Activity extends AppCompatActivity {
 
     @Override
